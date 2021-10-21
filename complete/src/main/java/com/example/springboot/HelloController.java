0291c37af836ed10ebde152f57cbf4a14c77456c
kimmel-nicholas <kimmel_nicholas@columbusstate.edu>
2021-10-21 03:23:50 +0000

    xkcd

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -3,6 +3,7 @@ package com.example.springboot;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.ui.Model;
 
 @Controller
@@ -18,5 +19,24 @@ public class HelloController {
 		model.addAttribute("name", name);
 		return "greeting";
 	}
+	@GetMapping("/xkcd")
+	public String xkcd(){
+		return "xkcd";
+	}
+	//Request parameter method
+	@GetMapping("/xkcdSpec")
+	public String xkcdSpecific(@RequestParam(name="id", required=false) String id, Model model) {
+		if(id == null){
+			id = "200";
+		}
+		model.addAttribute("id", id);
+		return "xkcdSpecific";
+	}
 
+	//Path variable method
+	// @GetMapping("/xkcdSpec/{id}")
+	// public String xkcdSpecific(@PathVariable String id, Model model) {
+	// 	model.addAttribute("id", id);
+	// 	return "xkcdSpecific";
+	// }
 }
